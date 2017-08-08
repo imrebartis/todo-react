956610ecdfc077a7990c903e9251dce75134aa07
imrebartis <imbartis@gmail.com>
2017-08-08 07:25:55 +0300

    addTodo adds todos to a new array (it doesn't change the startTodos array)

---
+++ src/lib/todoHelpers.js
@@ -0,0 +1,11 @@
+// this will pass only the first test (addTodo should add the passed todo to the list)
+// export const addTodo = (list, item) => {
+//     list.push(item);
+//     return list;
+// }
+
+
+// export const addTodo = (list, item) => list.concat(item)
+
+// another way to make sure we add todos to an array of its own (it doesn't change the startTodos array):
+export const addTodo = (list, item) => [...list, item]
