9fba030a58e43c2555fab7088652ee98308fa798
imrebartis <imbartis@gmail.com>
2017-08-08 18:04:01 +0300

    function composition enabled thru pipe

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,4 +4,12 @@
 // ...args in (null, ...args) is the spread operator
 // it spreads the array back out as arguments passed into bind
 
-export const partial = (fn, ...args) => fn.bind(null, ...args)
+export const partial = (fn, ...args) => fn.bind(null, ...args);
+
+// first ...arg (rest operator) = take whatever arg is passed in & wrap arguments up into an a array called arg
+// second ...args is spread
+const _pipe = (f, g) => (...args) => g(f(...args));
+
+// ...fns (rest operator) = take whatever functions are passed in & wrap them up into an a array called fns
+// then return instead of the arr of fns a single fn
+export const pipe = (...fns) => fns.reduce(_pipe);
