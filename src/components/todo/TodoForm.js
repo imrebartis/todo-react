cd588274b7a7cddb9a6f3d6df38c17d77e3915b4
imrebartis <imbartis@gmail.com>
2017-08-08 07:43:32 +0300

    new todos are added in the UI

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,20 +1,9 @@
 //STATELESS FUNCTIONAL COMPONENT
 import React from 'react';
 import PropTypes from 'prop-types';
-
-
-// the code here below doesn't work now that we've moved the form from App.js to this file
-//  PROPS TO THE RESCUE !!!! (+ see attributes of TodoForm in App.js)
-
-// export const TodoForm = () => (
-//   <form>
-//     <input type="text"
-//       onChange={this.handleInputChange}
-//       value={this.state.currentTodo}/>
-//   </form>)
         
 export const TodoForm = (props) => (
-  <form>
+  <form onSubmit={props.handleSubmit}>
     <input type="text"
       onChange={props.handleInputChange}
       value={props.currentTodo}/>
@@ -23,5 +12,6 @@ export const TodoForm = (props) => (
 // preventing the properties to be passed in a wrong datatype (currentTodo has to be a string, handleInputChange must be a function)
   TodoForm.propTypes = {
     currentTodo: PropTypes.string.isRequired,
-    handleInputChange: PropTypes.func.isRequired
+    handleInputChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired
   }
--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -8,4 +8,6 @@
 // export const addTodo = (list, item) => list.concat(item)
 
 // another way to make sure we add todos to an array of its own (it doesn't change the startTodos array):
-export const addTodo = (list, item) => [...list, item]
+export const addTodo = (list, item) => [...list, item];
+
+export const generateId = () => Math.floor(Math.random()*100000);
